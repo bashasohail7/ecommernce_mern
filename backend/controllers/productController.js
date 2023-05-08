e799a691a8b796740c40b71ef3b92e829eac0788
5682 <sohail.shaik@wisseninfotech.com>
2023-05-08 16:06:06 +0530

    product controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/products");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
 //create product -- admin
   exports.createProduct = catchAsyncErrors(async(req,res,next)=>{
     const product = await Product.create(req.body);
@@ -13,7 +14,11 @@ const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 
 //get all products
 exports.getAllProducts=catchAsyncErrors(async(req,res,next)=>{
-  const products = await Product.find();
+  const resultsPerPage=Number(req.query.limit);
+  // const resultsPerPage=1;
+  // console.log("resultsperpage query===>",req.query.resultsPerPage)
+  const apiFeature=new ApiFeatures(Product.find(),req.query).search().filter().pagination(resultsPerPage)
+  const products = await apiFeature.query;
   res.status(200).json({
     success: true,
     products,
@@ -23,6 +28,7 @@ exports.getAllProducts=catchAsyncErrors(async(req,res,next)=>{
 
 //only admin
 exports.updateProduct = catchAsyncErrors( async (req, res, next) => {
+  console.log('req ===>',req.params.id)
     const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true,
@@ -42,6 +48,7 @@ exports.deleteProduct =catchAsyncErrors( async (req, res, next) => {
 
 exports.getProductById = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
+ 
   res.status(200).json({
     success: true,
     product,
